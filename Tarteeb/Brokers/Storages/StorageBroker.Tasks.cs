63ff2ae390181e1865024a4dea5906bbbd26cbf2
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-17 21:49:17 +0500

    INFRA: Add-Migration

---
+++ Tarteeb/Brokers/Storages/StorageBroker.Tasks.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarteeb.Models.Tasks;
+
+namespace Tarteeb.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Models.Tasks.Task> Tasks { get; set; }
+    }
+}
