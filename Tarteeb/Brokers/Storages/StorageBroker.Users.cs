214ec2302f3132ef2bc4bf246f962938ebadcc12
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-08-23 09:33:43 +0500

    BROKERS: Insert User

---
+++ Tarteeb/Brokers/Storages/StorageBroker.Users.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+using Microsoft.EntityFrameworkCore;
+using Tarteeb.Models.Users;
+
+namespace Tarteeb.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        DbSet<User> Users { get; set; }
+    }
+}
