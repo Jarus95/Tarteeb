2c838a1112ae86c0f0c52740f9c4af078eacba99
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-09-03 21:58:18 +0500

    ShouldValidationExceptionOnAddIfInputNullAndLogItAsync -> WIP

--- Tarteeb.Tests/Services/Foundations/TicketServiceTests.Validations.Add.cs
+++ Tarteeb.Tests/Services/Foundations/TicketServiceTests.Validations.Add.cs
@@ -3,10 +3,17 @@
 // Free to use to bring order in your workplace
 //===============================
 
+using System.Threading.Tasks;
+using Xunit;
+
 namespace Tarteeb.Tests.Services.Foundations
 {
     public partial class TicketServiceTests
     {
+        [Fact]
+        public async Task ShouldValidationExceptionOnAddIfInputNullAndLogItAsync()
+        {
 
+        }
     }
 }
