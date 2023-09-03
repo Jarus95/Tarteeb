6b5804025e550fde05b6a254aaac3bee3843f52e
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2023-09-03 22:11:18 +0500

    BROKERS: Add Logging

---
+++ Tarteeb/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,20 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//===============================
+
+namespace Tarteeb.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger logger;
+        public LoggingBroker(ILogger logger) =>
+            this.logger = logger;
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception.Message, exception);
+
+    }
+}
